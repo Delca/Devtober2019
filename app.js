f0acb69bc0f398661cfa337789d43d7d1296f3c4
Grégoire <greg.duchemin@gmail.com>
2019-11-02 13:32:42 +0900

    Separated the templates from the index page

--- app.js
+++ app.js
@@ -27,7 +27,25 @@ console.log('app.js LOG');
 
 window.addEventListener('load', () => {
     console.log('app.js LOAD LOG');
+
     if (window.location.hostname !== 'localhost') {
         checkForVideoInput();
     }
+
+    var templatesToLoad = ['scannerModal', 'scannerResultModal'];
+    var parser = new DOMParser();
+
+    templatesToLoad.forEach((async templateName => {
+        let response = await fetch(`templates/${templateName}.html`);
+        let responseText = await response.text();
+        
+        if (response.status < 200 || response.status > 299) {
+            console.error(`Could not load template ${templateName} [${response.status}]: ${responseText}`);
+            return;
+        }
+
+        var template = parser.parseFromString(responseText, 'text/html').querySelector('head > template');
+        document.body.appendChild(template);
+        console.log(`Imported template ${templateName}`);
+    }));
 });
